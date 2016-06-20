7b80ddee1dd446335d39704fdf28f593990b23fa
Ariel Rodriguez <ariel@volonbolon.me>
2016-06-20 13:01:16 -0300

    setup flow

---
+++ CrispyLamp/CrispyLamp/EmptySegue.swift
@@ -0,0 +1,15 @@
+//
+//  EmptySegue.swift
+//  CrispyLamp
+//
+//  Created by Ariel Rodriguez on 6/20/16.
+//  Copyright © 2016 Ariel Rodriguez. All rights reserved.
+//
+
+import UIKit
+
+class EmptySegue:UIStoryboardSegue {
+    override func perform() {
+        super.perform()
+    }
+}
